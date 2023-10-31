112f369c24efee29744bddb06abb680617032b4a
Sofiapglez <usr3546@salesianos-lacuesta.net>
2023-10-31 10:09:01 +0000

    feat: files added and main structure done

--- App.tsx
+++ App.tsx
@@ -1,69 +1,109 @@
-import { useState } from 'react';
-import { Button, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, Image, Switch, TouchableOpacity } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 
+type Profile = {
+  name: string;
+  image: any;
+  about: string;
+  likes: string[];
+  github: string;
+  theme: {
+    backgroundColor: string;
+    textColor: string;
+  };
+};
+
+type Profiles = {
+  [key: string]: Profile;
+};
+
+const profiles: Profiles = {
+  persona1: {
+    name: "PABLO",
+    image: require('./assets/Pabloimage.png'),
+    about: "Soy Pablo Vadillo Núñez, me gustan el desarrollo y estoy en proceso de aprender aun más",
+    likes: ["Desarrollo Web", "Videojuegos", "Música"],
+    github: "https://github.com/Pablo-Vadillo",
+    theme: {
+      backgroundColor: 'white',
+      textColor: 'black',
+    },
+  },
+  persona2: {
+    name: "SOFIA",
+    image: require('./assets/Sofiaimage.jpg'),
+    about: "Soy Sofia, me gustan las ratas y estoy en proceso de tener más",
+    likes: ["Ratas", "Arte", "Literatura"],
+    github: "https://github.com/Sofiapglez",
+    theme: {
+      backgroundColor: 'white',
+      textColor: 'black',
+    },
+  },
+};
+
 export default function PortfolioApp() {
-  const [showProfile, setShowProfile] = useState(true);
+  const [selectedProfileKey, setSelectedProfileKey] = useState<keyof Profiles>('persona1');
+  const [darkMode, setDarkMode] = useState(false);
+  const [showLikes, setShowLikes] = useState(false);
+
+  const profile = profiles[selectedProfileKey];
+  const theme = darkMode ? { backgroundColor: 'black', textColor: 'white' } : profile.theme;
 
   return (
-    <View style={styles.mainContainer}>
+  
+    <View style={[styles.mainContainer, {backgroundColor: theme.backgroundColor}]}>
       <View style={styles.headerContainer}>
-        <Text style={styles.titleText}>Mi Portafolio</Text>
+        <Text style={[styles.titleText, {color: theme.textColor}]}>Mi Portafolio</Text>
         <View style={styles.navigationContainer}>
           <Button
-            onPress={() => setShowProfile(true)}
-            title="Mi Info"
+            onPress={() => setSelectedProfileKey('persona1')}
+            title="Pablo"
             color="gray"
           />
-          <Button
-            onPress={() => setShowProfile(false)}
-            title="Mi Repo"
+
+          <Button  
+            onPress={() => setSelectedProfileKey('persona2')}
+            title="Sofia"
             color="gray"
+          />
+
+          <Switch
+            onValueChange={() => setDarkMode(prev => !prev)}
+            value={darkMode}
           />
         </View>
       </View>
 
-      {showProfile ?
-        <View style={styles.profileContainer}>
-          <View style={styles.profileHeader}>
-            <Image style={styles.profileImage} source={require('./assets/SofyanAmrabat.jpg')} />
-            <View style={styles.descriptionContainer}>
-              <Text style={styles.descriptionTitle}>Sobre Mí</Text>
-              <Text>Soy profesor y me gusta mi trabajo, aunque a veces hago ejercicios complicados para mis estudiantes.</Text>
-            </View>
-          </View>
-          <Text style={styles.likesTitle}>Cosas que me gustan:</Text>
-          <ScrollView style={styles.likesList}>
-            <Text style={styles.likesItem}>Salir a pasear</Text>
-            <Text style={styles.likesItem}>Salir a pasear</Text>
-            <Text style={styles.likesItem}>Senderismo</Text>
-            <Text style={styles.likesItem}>Ir a la playa</Text>
-            <Text style={styles.likesItem}>Domingos de misa</Text>
-            <Text style={styles.likesItem}>Tocar la guitarra</Text>
-            <Text style={styles.likesItem}>Disfrutar de la naturaleza en días lluviosos</Text>
-            <Text style={styles.likesItem}>Viajar y explorar nuevos lugares</Text>
-            <Text style={styles.likesItem}>Escuchar música variada</Text>
-            <Text style={styles.likesItem}>Ver anime</Text>
-            <Text style={styles.likesItem}>Disfrutar de una ducha relajante</Text>
-            <Text style={styles.likesItem}>Jugar videojuegos</Text>
-            <Text style={styles.likesItem}>Cenas románticas</Text>
-            <Text style={styles.likesItem}>Aprender cosas nuevas</Text>
-            <Text style={styles.likesItem}>Practicar deportes</Text>
-            <Text style={styles.likesItem}>Leer libros interesantes</Text>
-            <Text style={styles.likesItem}>Cocinar platos deliciosos</Text>
-            <Text style={styles.likesItem}>Pasar tiempo con amigos y familia</Text>
-            <Text style={styles.likesItem}>Meditación y mindfulness</Text>
-            <Text style={styles.likesItem}>Fotografía</Text>
-            <Text style={styles.likesItem}>Arte y diseño creativo</Text>            
-          </ScrollView>
+      <View style={[styles.cardContainerStyle, { backgroundColor: darkMode ? 'black' : '#dbdbdb' }]}>
+        <View style={styles.profileHeader}>
+          <Image style={styles.profileImage} source={profile.image} />
+          <Text style={[styles.profileText, {color: theme.textColor}]}>{profile.name}</Text>
         </View>
-        :
-        <View style={styles.profileContainer}>
-          <View style={styles.qrContainer}>
-            <QRCode value="https://github.com/adhernea" />
+        <Text style={[styles.profileAbout, {color: theme.textColor}]}>{profile.about}</Text>
+
+        <TouchableOpacity
+          onPress={() => setShowLikes(!showLikes)}
+          style={styles.likesButton}
+        >
+          <Text style={[styles.likesButtonText, {color: theme.textColor}]}>Ver Gustos</Text>
+        </TouchableOpacity>
+
+        {showLikes && (
+          <View style={styles.likesContainer}>
+            {profile.likes.map((like, index) => (
+              <Text key={index} style={[styles.likeText, {color: theme.textColor}]}>
+                {like}
+              </Text>
+            ))}
           </View>
+        )}
+
+        <View style={styles.qrContainer}>
+          <QRCode value={profile.github} />
         </View>
-      }
+      </View>
     </View>
   );
 }
@@ -71,7 +111,6 @@ export default function PortfolioApp() {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -79,74 +118,70 @@ const styles = StyleSheet.create({
     height: '15%',
     paddingTop: 50,
     width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   titleText: {
-    backgroundColor: 'gray',
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 30,
   },
   navigationContainer: {
     flexDirection: 'row',
-    backgroundColor: 'darkgray',
     justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  profileContainer: {
-    width: '100%',
-    borderWidth: 2,
-    borderColor: 'black',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    height: '85%'
+    
   },
   profileHeader: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 20,
   },
   profileImage: {
-    height: 90,
-    width: 90,
-    borderRadius: 45
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    marginRight: 20,
   },
-  descriptionContainer: {
-    margin: 10,
-    backgroundColor: 'lightgray',
+  profileText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  profileAbout: {
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  likesButton: {
+    marginTop: 10,
     padding: 10,
-    borderRadius: 10,
-    width: '70%'
+    backgroundColor: '#b8b4b7',
+    borderRadius: 5,
   },
-  descriptionTitle: {
-    textAlign: 'center',
-    fontWeight: '700',
-    fontSize: 20
-  },
-  likesTitle: {
-    color: 'black',
-    fontWeight: "900",
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  likesList: {
-    padding: 10
-  },
-  likesItem: {
-    borderColor: 'black',
-    borderWidth: 1,
-    borderStyle: 'dashed',
-    padding: 20,
-    color: 'darkred',
+  likesButtonText: {
     textAlign: 'center',
     fontWeight: 'bold',
-    fontStyle: 'italic',
-    fontSize: 16,
-    backgroundColor: 'silver'
+  },
+  likesContainer: {
+    marginTop: 10,
+  },
+  likeText: {
+    textAlign: 'center',
   },
   qrContainer: {
     justifyContent: 'center',
-    borderWidth: 1,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 20,
+
+  },
+  cardContainerStyle: {
+    width: '80%',
+    borderWidth: 2,
+    borderColor: 'gray',
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 20,
   },
 });
