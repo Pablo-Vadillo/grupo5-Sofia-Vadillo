891b1f5d066940de7767116a33b29bef2ca669b5
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2023-10-31 09:55:57 +0100

    feat:Refactored Code

--- App.tsx
+++ App.tsx
@@ -1,94 +1,98 @@
 import { useState } from 'react';
-import { Button, Pressable, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 
-export default function App() {
-  const [displayMyQR, setDisplayMyQR] = useState(true);
+export default function PortfolioApp() {
+  const [showProfile, setShowProfile] = useState(true);
+
   return (
-    <View style={styles.container}>
-      <View style={styles.topContainer}>
-        <Text style={styles.firsttoprowContainer}>My Portfolio App</Text>
-        <View style={styles.rowTopSecondContainer}>
-        <Pressable style={styles.buttonruta} onPress={() => setDisplayMyQR(true)}>
-          <Text style={{...{color: 'white', fontWeight: 'bold', textTransform: 'uppercase'}, ...styles.shadoxboxing}}>Mi info</Text>
-        </Pressable>
-        <Button onPress={() => setDisplayMyQR(false)} title="Mi Repo" color="light-gray" accessibilityLabel='Un botón pal QR' />
+    <View style={styles.mainContainer}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.titleText}>Mi Portafolio</Text>
+        <View style={styles.navigationContainer}>
+          <Button
+            onPress={() => setShowProfile(true)}
+            title="Mi Info"
+            color="gray"
+          />
+          <Button
+            onPress={() => setShowProfile(false)}
+            title="Mi Repo"
+            color="gray"
+          />
         </View>
       </View>
-      {
-        displayMyQR ?
-          <View style={styles.bodystails}>
-            <View>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <Image style={styles.avatar} source={require('./assets/SofyanAmrabat.jpg')}></Image>
-              <View style={{margin: 10, backgroundColor: 'lightgray', padding: 10, borderRadius: 10, width: '70%'}}>
-            <Text style={{textAlign:'center', fontWeight: '700', fontSize: 20}}>
-            Descripción sobre mí!
-            </Text>
-            <Text>
-              Soy profe y me gusta mi trabajo aunque a veces me de por enrevesar prácticas para mis queridos alumnos
-            </Text>
-              </View>
-              </View>
-            <Text style= {{color: 'beriblak', fontWeight: "900", textTransform: 'capitalize', fontSize: 20, textAlign: 'center'}}>
-              cosas que me gustan mucho:
-            </Text>
-            <ScrollView style={{padding: 10}}>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Salir a pasear</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ir a la playita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Domingos de misa</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>La guitarrita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>El monte con lluvia</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Viajar</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Música variadita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Anime</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Videojuegos</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ir de cenar romántica</Text>
-            </ScrollView>
+
+      {showProfile ?
+        <View style={styles.profileContainer}>
+          <View style={styles.profileHeader}>
+            <Image style={styles.profileImage} source={require('./assets/SofyanAmrabat.jpg')} />
+            <View style={styles.descriptionContainer}>
+              <Text style={styles.descriptionTitle}>Sobre Mí</Text>
+              <Text>Soy profesor y me gusta mi trabajo, aunque a veces hago ejercicios complicados para mis estudiantes.</Text>
             </View>
           </View>
-          :
-              <View style={styles.bodystails}>
-            <View style={styles.CentrarcodigoQR}>
-          <QRCode value="https://github.com/adhernea" />
-            </View>
-              </View>
+          <Text style={styles.likesTitle}>Cosas que me gustan:</Text>
+          <ScrollView style={styles.likesList}>
+            <Text style={styles.likesItem}>Salir a pasear</Text>
+            <Text style={styles.likesItem}>Salir a pasear</Text>
+            <Text style={styles.likesItem}>Senderismo</Text>
+            <Text style={styles.likesItem}>Ir a la playa</Text>
+            <Text style={styles.likesItem}>Domingos de misa</Text>
+            <Text style={styles.likesItem}>Tocar la guitarra</Text>
+            <Text style={styles.likesItem}>Disfrutar de la naturaleza en días lluviosos</Text>
+            <Text style={styles.likesItem}>Viajar y explorar nuevos lugares</Text>
+            <Text style={styles.likesItem}>Escuchar música variada</Text>
+            <Text style={styles.likesItem}>Ver anime</Text>
+            <Text style={styles.likesItem}>Disfrutar de una ducha relajante</Text>
+            <Text style={styles.likesItem}>Jugar videojuegos</Text>
+            <Text style={styles.likesItem}>Cenas románticas</Text>
+            <Text style={styles.likesItem}>Aprender cosas nuevas</Text>
+            <Text style={styles.likesItem}>Practicar deportes</Text>
+            <Text style={styles.likesItem}>Leer libros interesantes</Text>
+            <Text style={styles.likesItem}>Cocinar platos deliciosos</Text>
+            <Text style={styles.likesItem}>Pasar tiempo con amigos y familia</Text>
+            <Text style={styles.likesItem}>Meditación y mindfulness</Text>
+            <Text style={styles.likesItem}>Fotografía</Text>
+            <Text style={styles.likesItem}>Arte y diseño creativo</Text>            
+          </ScrollView>
+        </View>
+        :
+        <View style={styles.profileContainer}>
+          <View style={styles.qrContainer}>
+            <QRCode value="https://github.com/adhernea" />
+          </View>
+        </View>
       }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  topContainer: {
+  headerContainer: {
     height: '15%',
     paddingTop: 50,
     width: '100%',
   },
-  firsttoprowContainer: {
+  titleText: {
     backgroundColor: 'gray',
     textAlign: 'center',
     fontWeight: 'bold',
-    textAlignVertical: 'center',
     fontSize: 30,
   },
-  rowTopSecondContainer: {
+  navigationContainer: {
     flexDirection: 'row',
     backgroundColor: 'darkgray',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center'
   },
-  buttonruta: {
-    width:'50%',
-  },
-  bodystails: {
+  profileContainer: {
     width: '100%',
     borderWidth: 2,
     borderColor: 'black',
@@ -96,12 +100,37 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     height: '85%'
   },
-  avatar: {
+  profileHeader: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  profileImage: {
     height: 90,
     width: 90,
-    borderRadius: 100
+    borderRadius: 45
   },
-  cosasQmeGustanMuxoEstails: {
+  descriptionContainer: {
+    margin: 10,
+    backgroundColor: 'lightgray',
+    padding: 10,
+    borderRadius: 10,
+    width: '70%'
+  },
+  descriptionTitle: {
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: 20
+  },
+  likesTitle: {
+    color: 'black',
+    fontWeight: "900",
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  likesList: {
+    padding: 10
+  },
+  likesItem: {
     borderColor: 'black',
     borderWidth: 1,
     borderStyle: 'dashed',
@@ -113,22 +142,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     backgroundColor: 'silver'
   },
-  CentrarcodigoQR: {
+  qrContainer: {
     justifyContent: 'center',
     borderWidth: 1,
     width: '100%',
     height: '100%',
     alignItems: 'center'
   },
-  shadoxboxing: {
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 7,
-    },
-    shadowOpacity: 0.43,
-    shadowRadius: 9.51,
-
-    elevation: 15,
-  }
 });
